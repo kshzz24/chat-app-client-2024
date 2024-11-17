6576733040892fbf74cd9e9327927a265f84118b
Kshitiz Bartaria <kshitizb02@gmail.com>
2024-11-18 00:10:50 +0530

    fix: resolve cors error

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,7 @@
 const corsOptions = {
-  origin: "*",
+  origin: [
+    "https://chat-app-2024-7l6qsz9du-nothing-matters-projects.vercel.app/",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
